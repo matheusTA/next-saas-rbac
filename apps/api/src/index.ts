d93c2aed95622cf088ab377abeb4bf39420edca7
matheusTA <matrajano12@gmail.com>
2024-04-19 23:17:00 -0300

    feat: configure auth package

---
+++ apps/api/src/index.ts
@@ -0,0 +1,9 @@
+import { defineAbilityFor } from '@saas/auth'
+
+const ability = defineAbilityFor({
+  role: 'MEMBER',
+})
+
+const userCanInviteSomeoneElse = ability.can('invite', 'User')
+
+console.log(userCanInviteSomeoneElse)
