e37c9f66db6d41534d72da356d0ccbba651dda10
matheusTA <matrajano12@gmail.com>
2024-06-08 21:46:31 -0300

    refactor: change conde style

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -7,9 +7,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
-  children: React.ReactNode
-}>) {
+}: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en" className="dark">
       <body>{children}</body>
